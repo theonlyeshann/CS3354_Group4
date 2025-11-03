a8b9bfa4973fd2dd147bef0d43800d41dab5f9c0
Eshann Saxena <eshannsaxena@Eshanns-MacBook-Air-2.local>
2025-11-03 12:03:37 -0600

    Created dashboard with clickable buttons

--- keysync/src/main.jsx
+++ keysync/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import LoginPage from './pages/loginpage.jsx'
+import Dashboard from './pages/dashboard.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LoginPage />
+    <Dashboard />
   </React.StrictMode>
 )
 
